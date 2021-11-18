091ff72eaa0d3e77abf071966ed30861adfebd36
Thibault <tibo.d37@gmail.com>
2021-11-18 09:43:25 +0100

    initial commit

---
+++ src/Entity/Commentaires.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CommentairesRepository;
+use Doctrine\ORM\Mapping as ORM;
+// Thibault DASSISE
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=CommentairesRepository::class)
+ */
+class Commentaires
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $auteur;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creeLe;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getCreeLe(): ?\DateTimeInterface
+    {
+        return $this->creeLe;
+    }
+
+    public function setCreeLe(\DateTimeInterface $creeLe): self
+    {
+        $this->creeLe = $creeLe;
+
+        return $this;
+    }
+}
